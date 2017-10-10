2114e2f8b9ab9a10235b13c64e0345ce182813b2
Eszter Judit Tóth <Eszter.Toth@betssongroup.com>
2017-10-10 13:14:38 +0200

    added getPetOwnerAllPets() to PetOwnerRestController

--- src/main/java/com/me/vetclinic/controller/PetOwnerRestController.java
+++ src/main/java/com/me/vetclinic/controller/PetOwnerRestController.java
@@ -1,26 +1,39 @@
 package com.me.vetclinic.controller;
 
+import com.me.vetclinic.domain.Pet;
 import com.me.vetclinic.domain.PetOwner;
 import com.me.vetclinic.service.PetOwnerService;
+import com.me.vetclinic.service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/petowner")
+@RequestMapping("/petOwner")
 public class PetOwnerRestController {
 
     private PetOwnerService petOwnerService;
+    private PetService petService;
 
     @Autowired
-    public PetOwnerRestController(PetOwnerService petOwnerService) {
+    public PetOwnerRestController(PetOwnerService petOwnerService, PetService petService) {
         this.petOwnerService = petOwnerService;
+        this.petService = petService;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{petOwnerId}")
     PetOwner getPetOwner(@PathVariable Long petOwnerId) {
         return petOwnerService.findById(petOwnerId);
     }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{petOwnerId}/pets")
+    List<Pet> getPetOwnerAllPets(@PathVariable Long petOwnerId) {
+        return petOwnerService.findById(petOwnerId).getPets();
+    }
+
 }
